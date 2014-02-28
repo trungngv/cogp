fa2f2f7ce76576c2eb4961cbbd33fafe7b1afd57
Trung Nguyen <trung.ngvan@gmail.com>
2014-02-28 17:12:09 +1100

    misc

--- src/scripts/toyme.m
+++ src/scripts/toyme.m
@@ -1,7 +1,7 @@
 clear all; clc; %close all;
-rng(111,'twister');
+%rng(111,'twister');
 
-CHOICE = 4;
+CHOICE = 5;
 switch CHOICE
   case 1
     % The function in SPGP 
@@ -35,13 +35,20 @@ switch CHOICE
     y = func(x) + sqrt(1e-2)*rand(size(x));
     func = 'sin(x) + cos(0.5*x)';
     theaxis = [-3 35 -3 3];
+  case 5
+    % jura
+    [x,y,xtest,ytest] = read_juraCd();
+    x = x(1:259,:); y = y(1:259,1);
+    ytest = ytest(:,1);
 end
 
-M = 10;
+M = 200;
 z0 = x(randperm(size(x,1),M),:);
 
 % fitc
-% [z,z0,~,mu,s2] = learn_fitc(x,y,xtest,M,z0);
+[z,z0,~,mu,s2] = learn_fitc(x,y,xtest,M,z0);
+disp('fitc mae = ')
+disp(mean(abs(mu-ytest)))
 % plot_all(x,y,xtest,mu,s2,z0,z,'FITC');
 % axis(theaxis);
 % saveas(gcf, ['results/figures/toy-fitc-run2.eps'],'epsc');
@@ -52,17 +59,19 @@ cf.covfunc = covfunc;
 cf.lrate     = 1e-2;
 cf.lrate_hyp = 1e-4;
 cf.lrate_beta = 1e-4;
-cf.lrate_z   = 1e-3;
+cf.lrate_z   = 1e-4;
 cf.momentum  = 0.9;
 cf.momentum_z = 0.0;
 cf.learn_z   = true;
 cf.init_kmeans = false;
 cf.maxiter = 1000;
-cf.nbatch = 50;
+cf.nbatch = 20;
 
 [mu,s2,elbo,params] = svi_learn(x,y,xtest,M,cf,z0);
-plot_all(x,y,xtest,mu,s2,params.z0,params.z,['GPSVI ' func ' iter = 100']);
-axis(theaxis);
+disp('svigp fitc mae = ')
+disp(mean(abs(mu-ytest)))
+%plot_all(x,y,xtest,mu,s2,params.z0,params.z,['GPSVI ' func ' iter = 100']);
+%axis(theaxis);
 %saveas(gcf, ['results/figures/toy-iter' num2str(cf.maxiter) '-nbatch' num2str(cf.nbatch) '-run2.eps'],'epsc');
 
 figure;
