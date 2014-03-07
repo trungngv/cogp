c7b3eeb9e8fb2ac752a6610ea1778f9278275565
Trung Nguyen <trung.ngvan@gmail.com>
2014-03-07 17:22:12 +1100

    script for running fx and weather

--- src/scripts/slfm_fx.m
+++ src/scripts/slfm_fx.m
@@ -1,3 +1,4 @@
+%% reading data and model setting
 clear all; %clc; %close all;
 rng(1110,'twister');
 
@@ -5,7 +6,6 @@ rng(1110,'twister');
 [x,y,xtest,ytest,y0] = read_fx();
 [y,ymean,ystd] = standardize(y,[],[]);
 
-%% ssvi
 cf.covfunc_g  = 'covSEard';
 cf.lrate      = 1e-2;
 cf.momentum   = 0.9;
@@ -16,17 +16,25 @@ cf.lrate_w    = 1e-5;
 cf.learn_z    = false;
 cf.momentum_z = 0.0;
 cf.lrate_z    = 1e-4;
-cf.init_kmeans = false;
 cf.maxiter = 500;
 cf.nbatch = 200;
 cf.beta = 1/0.1;
 cf.initz = 'random';
-
+cf.w = ones(size(y,2),2);
+cf.monitor_elbo = 50;
+cf.fix_first = false;
 M = 100;
-Q = 1;
+Q = 2;
+xtest = cell(3,1);
+xtest{1} = (50:100)';
+xtest{2} = (100:150)';
+xtest{3} = (150:200)';
+outputs = [4,6,9];
+
+%% single run 
 par.g = cell(Q,1);
 [elbo,par] = slfm2_learn(x,y,M,par,cf);
-[mu,var,mu_g,var_g] = slfm2_predict(cf.covfunc_g,par,xtest,size(y,2));
+[mu,var,mu_g,var_g] = slfm2_predict(cf.covfunc_g,par,x,size(y,2));
 mu =mu.*repmat(ystd,size(mu,1),1) + repmat(ymean,size(mu,1),1);
 
 disp('elbo = ')
@@ -36,18 +44,13 @@ disp(par.w)
 
 figure;
 %plot(1:numel(elbo),elbo);
+elbo = elbo(elbo ~= 0);
 semilogy(1:numel(elbo),-elbo);
 ylabel('elbo')
 xlabel('iteration')
 title(['elbo vs. iteration, lrate = ' num2str(cf.lrate_z)])
 
-%%
-outputs = [4,6,9];
 titles = {'CAD','JPY','AUD'};
-xtest = cell(3,1);
-xtest{1} = (50:100)';
-xtest{2} = (100:150)';
-xtest{3} = (150:200)';
 disp('smse of CAD, JPY, AUD')
 theaxis{1} = [0 250 0.75 1.15];
 theaxis{2} = [0 250 7.8*1e-3 9.4*1e-3];
@@ -56,10 +59,29 @@ smses = zeros(3,1);
 for i=1:3
   t = outputs(i);
   plot_all(x,y0(:,t),x,mu(:,t),(ystd(t).^2)*var(:,t),[],[],titles{i});
+  plot(xtest{i},y0(xtest{i},t),'.b','MarkerSize',14); % override the magenta color for missing data
   axis(theaxis{i});
+  saveas(gcf, ['results/figures/fx' titles{i} '.eps'],'epsc');
   smses(i) = mysmse(ytest(xtest{i},t),mu(xtest{i},t),ymean(t));
 end  
 disp(smses)
 disp('average = ')
 disp(mean(smses))
+
+%% batch for slfm_fx
+runs = 10;
+smses = zeros(runs,3);
+for r=1:runs
+  par.g = cell(Q,1);
+  [elbo,par] = slfm2_learn(x,y,M,par,cf);
+  [mu,var,mu_g,var_g] = slfm2_predict(cf.covfunc_g,par,x,size(y,2));
+  mu =mu.*repmat(ystd,size(mu,1),1) + repmat(ymean,size(mu,1),1);
+  for i=1:3
+    t = outputs(i);
+    smses(r,i) = mysmse(ytest(xtest{i},t),mu(xtest{i},t),ymean(t));
+  end  
+end
+save('fx-slfm','smses');
+disp('mean smses')
+disp(mean(smses(:)))
 
