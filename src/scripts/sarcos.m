12442234a9ada9665e077a8feca6ee04c6e4bd71
Trung Nguyen <trung.ngvan@gmail.com>
2014-03-07 17:42:58 +1100

    script to run sarcos

--- src/scripts/sarcos.m
+++ src/scripts/sarcos.m
@@ -18,46 +18,46 @@ clear sarcos_inv
 clear sarcos_inv_test
 
 %% Exploratory analysis of sarcos
-correlation = zeros(7,7);
-for i=1:7
-  for j=1:7
-    correlation(i,j) = corr(y(:,i),y(:,j));
-  end
-end
-
-disp('correlation: ')
-correlation
-
-outputs = [2,3,4,7];
-for i=1:numel(outputs)-1
-  for j=i+1:numel(outputs)
-    figure; scatter(y(:,i),y(:,j)); title(['output ' num2str(i) ' and ' num2str(j)']);
-  end
-end
+% correlation = zeros(7,7);
+% for i=1:7
+%   for j=1:7
+%     correlation(i,j) = corr(y(:,i),y(:,j));
+%   end
+% end
+% 
+% disp('correlation: ')
+% correlation
+% 
+% outputs = [2,3,4,7];
+% for i=1:numel(outputs)-1
+%   for j=(i+1):numel(outputs)
+%     figure; scatter(y(:,i),y(:,j)); title(['output ' num2str(i) ' and ' num2str(j)']);
+%   end
+% end
 
 %% independent models - fitc
-rng(1110,'twister');
-M = 200;
-runs = 5;
-maes = zeros(runs,size(y,2));
-rmses = maes;
-smses = maes;
-for n=1:runs
-  for i=1:size(y,2)
-    [z,z0,~,mu,s2] = learn_fitc(x,y(:,i),xtest,M,[]);
-    mu = mu*ystd(i)+ymean(i);
-    disp('mae    rmse')
-    fprintf('%.4f   %.4f\n', fmae(ytest(:,i),mu), frmse(ytest(:,i),mu));
-    maes(n,i) = fmae(ytest(:,i),mu);
-    rmses(n,i) = frmse(ytest(:,i),mu);
-    smses(n,i) = mysmse(ytest(:,i),mu,ymean(i));
-  end  
-end  
-save(['sarcos-fitc-M' num2str(M)],'maes','rmses','smses');
-disp('average mae, rmse, smse')
-disp(mean(maes))
-disp(mean(rmses))
-disp(mean(smses))
+% rng(1110,'twister');
+% M = 200;
+% runs = 5;
+% maes = zeros(runs,size(y,2));
+% rmses = maes;
+% smses = maes;
+% for n=1:runs
+%   for i=1:size(y,2)
+%     [z,z0,~,mu,s2] = learn_fitc(x,y(:,i),xtest,M,[]);
+%     mu = mu*ystd(i)+ymean(i);
+%     disp('mae    rmse')
+%     fprintf('%.4f   %.4f\n', fmae(ytest(:,i),mu), frmse(ytest(:,i),mu));
+%     maes(n,i) = fmae(ytest(:,i),mu);
+%     rmses(n,i) = frmse(ytest(:,i),mu);
+%     smses(n,i) = mysmse(ytest(:,i),mu,ymean(i));
+%   end  
+% end  
+% save(['sarcos-fitc-M' num2str(M)],'maes','rmses','smses');
+% disp('average mae, rmse, smse')
+% disp(mean(maes))
+% disp(mean(rmses))
+% disp(mean(smses))
 
 %% independent models - svigp
 rng(1110,'twister');
@@ -70,28 +70,53 @@ cf.lrate_z   = 1e-4;
 cf.momentum  = 0.9;
 cf.momentum_z = 0.0;
 cf.learn_z   = true;
-cf.init_kmeans = true;
-cf.maxiter = 100;
-cf.nbatch = 1000;
+cf.maxiter = 1000;
+cf.nbatch = 5000;
+cf.beta = 1/0.01;
+cf.initz = 'random';
+cf.monitor_elbo = 50;
 
-M = 500;
+M = 200;
 runs = 1;
 maes = zeros(runs,size(y,2));
-rmses = maes;
+smses = maes;
 for n=1:runs
   for i=1:size(y,2)
     [mu,s2,elbo,params] = svi_learn(x,y(:,i),xtest,M,cf,[]);
     mu = mu*ystd(i)+ymean(i);
-    disp('mae    rmse')
+    disp('mae    smse')
     fprintf('%.4f   %.4f\n', fmae(ytest(:,i),mu), frmse(ytest(:,i),mu));
     maes(n,i) = fmae(ytest(:,i),mu);
-    rmses(n,i) = frmse(ytest(:,i),mu);
+    smses(n,i) = mysmse(ytest(:,i),mu,ymean(i));
   end  
 end  
 
-disp('average mae and rmse')
+disp('average mae and smses')
 disp(mean(maes))
-disp(mean(rmses))
+disp(mean(smses))
 
-%% structure model - slfm
-slfm_sarcos
+%% independent model - soD
+% rng(1110,'twister');
+% runs = 5;
+% maes = zeros(runs,size(y,2));
+% rmses = maes;
+% smses = maes;
+% for n=1:runs
+%   for i=1:size(y,2)
+%     randind = randperm(size(x,1),2000);
+%     xsod = x(randind,:);
+%     ysod = y(randind,i);
+%     model = standard_gp([],xsod,ysod,xtest,[],false);
+%     mu = model.fmean*ystd(i)+ymean(i);
+%     disp('mae    rmse')
+%     fprintf('%.4f   %.4f\n', fmae(ytest(:,i),mu), frmse(ytest(:,i),mu));
+%     maes(n,i) = fmae(ytest(:,i),mu);
+%     rmses(n,i) = frmse(ytest(:,i),mu);
+%     smses(n,i) = mysmse(ytest(:,i),mu,ymean(i));
+%   end  
+% end  
+% save(['sarcos-sod'],'maes','rmses','smses','model');
+% disp('average mae, rmse, smse')
+% disp(mean(maes))
+% disp(mean(rmses))
+% disp(mean(smses))
